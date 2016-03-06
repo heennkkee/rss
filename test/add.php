e16d5bf7db4aae2d22cd5757b68006ec812a039c
Henrik Aronsson <henrik.aronsson.94@gmail.com>
2016-03-06 16:08:53 +0100

    Added files via upload

---
+++ test/add.php
@@ -0,0 +1,21 @@
+<?php
+if (isset($_POST['TITLE'])) {
+  include('../src/Crss.php');
+  $rss = new \henaro\rss\Crss();
+  $rss->insertRSS(['LINK' => $_POST['LINK'], 'TITLE' => $_POST['TITLE'], 'DESCRIPTION' => $_POST['DESCRIPTION']]);
+  Header('Location: ' . $_SERVER['PHP_SELF']);
+}
+?>
+<html>
+<body>
+  <p><a href="rss.php">View rss feed</a><p>
+  <fieldset>
+    <form action="?" method="POST">
+      <p><input required name="TITLE" type="text" placeholder="Title"></p>
+      <p><input required name="LINK" type="url" placeholder="Link"></p>
+      <p><input required name="DESCRIPTION" type="text" placeholder="Description"></p>
+      <input type="submit">
+    </form>
+  </fieldset>
+</body>
+</html>
