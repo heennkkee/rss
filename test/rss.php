57bef14c8ca48a47fff7e4ffc45530264f5248ee
Henrik Aronsson <henrik.aronsson.94@gmail.com>
2016-03-06 16:09:05 +0100

    Delete rss.php

--- test/rss.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include('../src/Crss.php');
-
-$rss = new \henaro\rss\Crss();
-
-$rss->getRSS();
-
-//$rss->insertRSS(['LINK' => 'http://www.henrik.com/', 'TITLE' => 'Henrik post', 'DESCRIPTION' => 'Test av Henriks sida']);
