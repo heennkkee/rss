69809a46f62272217950c21554763f9874eecebd
Henrik Aronsson <henrik.aronsson.94@gmail.com>
2016-03-06 16:11:28 +0100

    Added files via upload

--- test/rss.php
+++ test/rss.php
@@ -5,5 +5,3 @@ include('../src/Crss.php');
 $rss = new \henaro\rss\Crss();
 
 $rss->getRSS();
-
-//$rss->insertRSS(['LINK' => 'http://www.henrik.com/', 'TITLE' => 'Henrik post', 'DESCRIPTION' => 'Test av Henriks sida']);
